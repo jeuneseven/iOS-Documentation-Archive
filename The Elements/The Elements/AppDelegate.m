a6b1200ce98100d6024c2388350dfc1e84b3c36c
kkk <jeuneseven@gmail.com>
2017-08-10 16:36:17 +0800

    移除SB；手写代码实现

--- The Elements/The Elements/AppDelegate.m
+++ The Elements/The Elements/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,14 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    self.window.rootViewController = [ViewController new];
+    
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
