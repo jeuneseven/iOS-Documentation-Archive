30c7bc3599f9b4e78a6d4d62ee0a0388ff789f0e
kkk <jeuneseven@gmail.com>
2017-08-10 16:59:32 +0800

    创建tabbar、nav

--- The Elements/The Elements/AppDelegate.m
+++ The Elements/The Elements/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "ViewController.h"
+#import "ElementsTableViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,37 +17,30 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     
-    self.window.rootViewController = [ViewController new];
+    UITabBarController *tabbarController = [UITabBarController new];
+    
+    ElementsTableViewController *nameVC = [[ElementsTableViewController alloc] init];
+    UINavigationController *nameNavigationController = [[UINavigationController alloc] initWithRootViewController:nameVC];
+    
+    ElementsTableViewController *atomicNumberVC = [[ElementsTableViewController alloc] init];
+    UINavigationController *atomicNumberNavigationController = [[UINavigationController alloc] initWithRootViewController:atomicNumberVC];
+    
+    ElementsTableViewController *symbolVC = [[ElementsTableViewController alloc] init];
+    UINavigationController *symbolNavigationController = [[UINavigationController alloc] initWithRootViewController:symbolVC];
+    
+    ElementsTableViewController *stateVC = [[ElementsTableViewController alloc] init];
+    UINavigationController *stateNavigationController = [[UINavigationController alloc] initWithRootViewController:stateVC];
+    
+    tabbarController.viewControllers = @[nameNavigationController, atomicNumberNavigationController, symbolNavigationController, stateNavigationController];
+    
+    self.window.rootViewController = tabbarController;
     
     [self.window makeKeyAndVisible];
     
     return YES;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
---
+++ The Elements/The Elements/ViewControllers/ElementsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ElementsTableViewController.h
+//  The Elements
+//
+//  Created by 李占昆 on 2017/8/10.
+//  Copyright © 2017年 kkk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ElementsTableViewController : UITableViewController
+
+@end
