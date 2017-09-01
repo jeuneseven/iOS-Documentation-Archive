b1600f6d9a4ffe7139a58a972e91d8467007706f
kkk <jeuneseven@gmail.com>
2017-09-01 14:49:21 +0800

    增加AtomicElementViewController

---
+++ The Elements/The Elements/ViewControllers/AtomicElementViewController.h
@@ -0,0 +1,16 @@
+//
+//  AtomicElementViewController.h
+//  The Elements
+//
+//  Created by 李占昆 on 2017/9/1.
+//  Copyright © 2017年 kkk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AtomicElement;
+@interface AtomicElementViewController : UIViewController
+
+@property (nonatomic) AtomicElement *element;
+
+@end
---
+++ The Elements/The Elements/ViewControllers/AtomicElementViewController.m
@@ -0,0 +1,14 @@
+//
+//  AtomicElementViewController.m
+//  The Elements
+//
+//  Created by 李占昆 on 2017/9/1.
+//  Copyright © 2017年 kkk. All rights reserved.
+//
+
+#import "AtomicElementViewController.h"
+#import "AtomicElement.h"
+
+@implementation AtomicElementViewController
+
+@end
